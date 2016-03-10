4d475198612dad06f5ecd5dc894efc0adf49f36d
Neil Gannon <neil.gannon@gmail.com>
2016-03-10 15:54:45 +0000

    Merged Projects

---
+++ Assets/Scripts/HealthUi.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthUi : MonoBehaviour
+{
+    public Slider slider;
+
+    // Use this for initialization
+    void Start()
+    {
+        MessageHandler msgHandler = GetComponent<MessageHandler>();
+
+        if (msgHandler)
+        {
+            msgHandler.RegisterDelegate(RecieveMessage);
+        }
+    }
+
+
+    void RecieveMessage(MessageType msgType, GameObject go, MessageData msgData)
+    {
+        switch (msgType)
+        {
+            case MessageType.HEALTHCHANGED:
+                HealthData hpData = msgData as HealthData;
+
+                if (hpData != null)
+                {
+                    UpdateUi(hpData.maxHealth, hpData.curHealth);
+                }
+                break;
+        }
+    }
+
+    void UpdateUi(int maxHealth, int curHealth)
+    {
+        slider.value = (1.0f / maxHealth) * curHealth;
+    }
+}
